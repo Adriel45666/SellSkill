b665f7e931c926419cb70c20d9f3f59d5a146013
Adriel45666 <adriel_g_gomes@estudante.sesisenai.org.br>
2025-06-30 22:18:22 -0300

    Add files via upload

---
+++ ProjSellSkill/BackEnd/SellSkill/src/main/java/com/ska/SellSkill/repository/custom/UsuarioRepositoryCustom.java
@@ -0,0 +1,8 @@
+
+package com.ska.SellSkill.repository.custom;
+
+import com.ska.SellSkill.dto.UsuarioDTO;
+
+public interface UsuarioRepositoryCustom {
+     public boolean inserirUsuario(UsuarioDTO usuarioDTO);
+}
