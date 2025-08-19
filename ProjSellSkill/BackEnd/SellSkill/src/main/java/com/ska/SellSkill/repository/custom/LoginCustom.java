e5eeeaa928a7427f4732efd6ed7c419b6c1447ea
Adriel45666 <adriel_g_gomes@estudante.sesisenai.org.br>
2025-08-18 21:42:19 -0300

    Delete ProjSellSkill directory

--- ProjSellSkill/BackEnd/SellSkill/src/main/java/com/ska/SellSkill/repository/custom/LoginCustom.java
+++
@@ -1,7 +0,0 @@
-package com.ska.SellSkill.repository.custom;
-
-import com.ska.SellSkill.model.LoginModel;
-
-public interface LoginCustom {
-    LoginModel buscarPorEmailESenha(String email, String senha);
-}
--- ProjSellSkill/BackEnd/SellSkill/src/main/java/com/ska/SellSkill/repository/custom/UsuarioRepositoryCustom.java
+++
@@ -1,7 +0,0 @@
-package com.ska.SellSkill.repository.custom;
-
-import com.ska.SellSkill.dto.CadastroDTO;
-
-public interface UsuarioRepositoryCustom {
-    boolean inserirUsuario(CadastroDTO dto);
-}
