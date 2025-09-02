8e90e6b525716f7878166fe2df9e15799cb71ab9
Adriel45666 <adriel_g_gomes@estudante.sesisenai.org.br>
2025-09-01 21:59:44 -0300

    Delete ProjSellSkill/BackEnd/SellSkill directory

--- ProjSellSkill/BackEnd/SellSkill/src/main/java/com/ska/SellSkill/repository/custom/UsuarioRepositoryCustom.java
+++
@@ -1,8 +0,0 @@
-
-package com.ska.SellSkill.repository.custom;
-
-import com.ska.SellSkill.dto.UsuarioDTO;
-
-public interface UsuarioRepositoryCustom {
-     public boolean inserirUsuario(UsuarioDTO usuarioDTO);
-}
